082ff294f83018e83d615035a5fb3e8b01c933e9
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2023-06-11 12:36:48 -0500

    Add controller for new workout and other stuff

---
+++ src/services/workoutService.js
@@ -0,0 +1,38 @@
+const Workout = require("../database/Workout");
+const { v4: uuid } = require("uuid");
+
+const getAllWorkouts = () => {
+  const allWorkouts = Workout.getAllWorkouts();
+  return allWorkouts;
+};
+
+const getOneWorkout = () => {
+  return;
+};
+
+const createNewWorkout = (newWorkout) => {
+  const workoutToInsert = {
+    ...newWorkout,
+    id: uuid(),
+    createdAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
+    updatedAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
+  };
+  const createdWorkout = Workout.createNewWorkout(workoutToInsert);
+  return createdWorkout;
+};
+
+const updateOneWorkout = () => {
+  return;
+};
+
+const deleteOneWorkout = () => {
+  return;
+};
+
+module.exports = {
+  getAllWorkouts,
+  getOneWorkout,
+  createNewWorkout,
+  updateOneWorkout,
+  deleteOneWorkout,
+};
